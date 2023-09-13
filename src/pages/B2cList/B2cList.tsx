56ef99204d5b7c06fe7fc353a4986fcccd06489c
Mouhamadou-Soumare <82391111+Mouhamadou-Soumare@users.noreply.github.com>
2023-09-13 22:57:35 +0200

    correction errors

--- src/pages/B2cList/B2cList.tsx
+++ src/pages/B2cList/B2cList.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Box from '@mui/material/Box';
 import TitleAtom from '../../components/atoms/Title/title';
-import TableDashboard from '../../components/organisms/TableDashboard/TableDashboard';
+import TableDashboard from '../../components/organisms/TableDashboard/TableDashboard'
 import ButtonAtom from '../../components/atoms/Button/button';
 import "./b2clist.scss";
 
